a93de914ece91c906fb99b257ba30fbce2bbea94
Olya_Polyanskaya <polyanskaya3000@gmail.com>
2022-01-09 23:28:51 +0700

    feat: finish task Human Readable Number

--- src/index.js
+++ src/index.js
@@ -1,3 +1,33 @@
 module.exports = function toReadable (number) {
-  
+  const massivNumber = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety', 'hundred'];
+  if ( number < 21 ) {
+    return massivNumber[number];
+  } else if (number > 20 && number < 100) {
+      let firstNumber = Math.floor(number / 10);
+      let secondNumber = number - firstNumber * 10;
+
+      if (secondNumber === 0) {
+        return `${massivNumber[18 + firstNumber]}`;
+    }
+    return `${massivNumber[18 + firstNumber]} ${massivNumber[secondNumber]}`;
+  } else {
+       let firstNumber = Math.floor(number / 100);
+       let secondNumber = Math.floor((number % 100) / 10);
+       let thirdNumber = number % 10;
+
+       if (number % 100 < 21 && number % 100 !== 0 ) {
+           return `${massivNumber[firstNumber]} ${massivNumber[28]} ${massivNumber[number % 100]}` ;
+       }
+       if (thirdNumber === 0 && secondNumber === 0) {
+        return `${massivNumber[firstNumber]} ${massivNumber[28]}`;
+       }
+       if (thirdNumber === 0) {
+        return `${massivNumber[firstNumber]} ${massivNumber[28]} ${massivNumber[18 + secondNumber]}`;
+       } 
+       if (secondNumber === 0) {
+        return `${massivNumber[firstNumber]} ${massivNumber[28]} ${massivNumber[thirdNumber]}` ;
+       }
+       return `${massivNumber[firstNumber]} ${massivNumber[28]} ${massivNumber[18 + secondNumber]} ${massivNumber[thirdNumber]}`
+  }
 }
+
